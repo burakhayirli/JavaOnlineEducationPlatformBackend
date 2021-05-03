6786f4aa1fc16b2c5745f7b2d2983e8fc79ca0ed
Dev Environment <devenvironment@hotmail.com>
2021-05-03 12:49:41 +0300

    Customer sınıfı oluşturuldu. toString metodu override edildi. Manager sınıfları ve interfaceler oluşturuldu. Main metodu içinde ilk müşteri oluştularak manager aracılığıyla ekrana bilgiler basıldı.

--- src/ders4Odev2_musteriYonetimSistemi/Main.java
+++ src/ders4Odev2_musteriYonetimSistemi/Main.java
@@ -6,10 +6,23 @@
  */
 package ders4Odev2_musteriYonetimSistemi;
 
+import java.sql.Date;
+
+import ders4Odev2_musteriYonetimSistemi.Abstract.BaseCustomerManager;
+import ders4Odev2_musteriYonetimSistemi.Concrete.NeroCustomerManager;
+import ders4Odev2_musteriYonetimSistemi.Concrete.StarbucksCustomerManager;
+import ders4Odev2_musteriYonetimSistemi.Entities.Customer;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		BaseCustomerManager customerManager= new StarbucksCustomerManager();
+		Customer customer = new Customer();
+		customer.setFirstName("Burak");
+		customer.setLastName("Hayýrlý");
+		customer.setDateOfBirth(new Date(1984,1,1));
+		customer.setNationalityId("1234567890");
+		customerManager.save(customer);
 	}
 
 }
