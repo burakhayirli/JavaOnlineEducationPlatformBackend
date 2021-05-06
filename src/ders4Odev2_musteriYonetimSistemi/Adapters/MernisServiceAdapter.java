96ce39f94aeb6dcee56943cb8aa7d30d295a97cf
Dev Environment <devenvironment@hotmail.com>
2021-05-06 11:33:36 +0300

    Last Commit

--- src/ders4Odev2_musteriYonetimSistemi/Adapters/MernisServiceAdapter.java
+++ src/ders4Odev2_musteriYonetimSistemi/Adapters/MernisServiceAdapter.java
@@ -10,6 +10,7 @@ import java.rmi.RemoteException;
 
 import ders4Odev2_musteriYonetimSistemi.Abstract.CustomerCheckService;
 import ders4Odev2_musteriYonetimSistemi.Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoap;
 import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 public class MernisServiceAdapter implements CustomerCheckService {
@@ -20,8 +21,9 @@ public class MernisServiceAdapter implements CustomerCheckService {
 		//Ancak þimdilik Algoritma ile T.C. Kimlik No kontrol edelim.
 		//return isTCKNCorrect(customer.getNationalityId());
 		
-		KPSPublicSoapProxy client=new KPSPublicSoapProxy();
 		boolean isValidated=false;
+	
+		KPSPublicSoapProxy client=new KPSPublicSoapProxy();
 		try {
 			isValidated= client.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()), 
 					customer.getFirstName(),customer.getLastName(), customer.getDateOfBirth().getYear());
@@ -30,39 +32,7 @@ public class MernisServiceAdapter implements CustomerCheckService {
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
-		
+
 		return isValidated;
-		
 	}
-
-	
-	
-	private boolean isTCKNCorrect(String id) {
-		if (id == null)
-			return false;
-
-		if (id.length() != 11)
-			return false;
-
-		char[] chars = id.toCharArray();
-		int[] a = new int[11];
-
-		for (int i = 0; i < 11; i++) {
-			a[i] = chars[i] - '0';
-		}
-
-		if (a[0] == 0)
-			return false;
-		if (a[10] % 2 == 1)
-			return false;
-
-		if (((a[0] + a[2] + a[4] + a[6] + a[8]) * 7 - (a[1] + a[3] + a[5] + a[7])) % 10 != a[9])
-			return false;
-
-		if ((a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9]) % 10 != a[10])
-			return false;
-
-		return true;
-	}
-
 }
